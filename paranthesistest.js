ebb21ca67d47b012641fe590c72d7c1527c1dccc
andylnkd <dhalwani1@gmail.com>
2022-09-23 22:55:45 +0000

    paranthensis tests

---
+++ paranthesistest.js
@@ -0,0 +1,9 @@
+// console.log('hey , getting started with this test to see how es6 treats paranthesis()')
+
+const i = 1+2;
+
+
+// console.log(`hey , getting started with this test to see how es6 treats paranthesis()- first, ${i}`)
+console.log(`hey 👋 ${i}`)
+i==`1` ? console.log(`in the ? part of the ternary operator`) : 
+{if (i) {console.log(`hey 👋 ${i}`)}}
