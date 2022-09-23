48475ce1ea96fd86556d7af95cf9e7fb14e6c0e0
andylnkd <dhalwani1@gmail.com>
2022-09-23 23:09:59 +0000

    solved the function part, rest is just c format, if condition or return values

--- paranthesistest.js
+++ paranthesistest.js
@@ -1,10 +1,12 @@
 // console.log('hey , getting started with this test to see how es6 treats paranthesis()')
 
-const i = 1+2;
+// const i = 1+2;
 
 
-// console.log(`hey , getting started with this test to see how es6 treats paranthesis()- first, ${i}`)
-console.log(`hey 👋 ${i}`)
-i==`1` ? (console.log(`in the ? 
-part of the ternary operator`)) : 
-{if (i) {console.log(`hey 👋 ${i}`)}}
+// // console.log(`hey , getting started with this test to see how es6 treats paranthesis()- first, ${i}`)
+// // console.log(`hey 👋 ${i}`)
+// i+2==5 ? console.
+//     log(`in the ? part of the ternary operator`) : 
+// {if (i) {console.log(`hey 👋 ${i}`)}}
+
+(console.log)('hey - testing out this cool format')//Got this ! This is really a function...man I love this!
