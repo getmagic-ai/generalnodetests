33f557207fcecfc214de32de0d770a1c63e641fe
andylnkd <dhalwani1@gmail.com>
2022-09-24 01:36:42 +0000

    Just some more stuff and fun! Please enter the commit message for your changes. Lines starting

---
+++ specialfunctions.js
@@ -0,0 +1,10 @@
+//special functions
+
+const str = `test string here! excited to get 
+(this was a new line) going!`
+const toUpper = (a) => a.toUpperCase
+console.log(typeof(toUpper))
+console.log(toUpper(((str.split('!')))))
+
+
+if(str) 1+1
