4ec6f682a66c6451086ba65dfcf0c12958239feb
andylnkd <dhalwani1@gmail.com>
2022-09-24 02:55:06 +0000

    not worth the complexity but good to go in depth

--- specialfunctions.js
+++ specialfunctions.js
@@ -1,10 +1,2 @@
 //special functions
 
-const str = `test string here! excited to get 
-(this was a new line) going!`
-const toUpper = (a) => a.toUpperCase
-console.log(typeof(toUpper))
-console.log(toUpper(((str.split('!')))))
-
-
-if(str) 1+1
